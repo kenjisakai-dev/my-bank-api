a20350311653d6c9b4b3d56ffde75180a67ba66b
Kenji Sakai <kenjisakai80@gmail.com>
2023-04-26 18:14:46 -0300

    3.5 Método GET por id

--- routes/account.route.js
+++ routes/account.route.js
@@ -31,4 +31,16 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const account = data.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ erro: err.message });
+  }
+});
+
 export default router;
